69848d96fbaf33d44cc3470fba8fd3b8879a36c0
Vinícius Conde Vasconcelos <136513095+Vinyyciws@users.noreply.github.com>
2024-08-12 19:59:56 -0300

    3 alterações de validação

--- exercicios/aula16ex/ex018/script.js
+++ exercicios/aula16ex/ex018/script.js
@@ -28,38 +28,46 @@ function add() {
             optArr.innerText = `${val[key]}. Valor adicionado`
         }
         sel.appendChild(optArr)
+        res.innerHTML = ''
     } else {
         alert('Dados inválidos ou já existentes. Verifique os valores inseridos!')
     }
+    num.value = ''
+    num.focus()
 }
 
 function finalizar() {
-    res.innerHTML = ''
-    // Total de números
-    let totalNum = document.createElement('p')
-    res.appendChild(totalNum)
-    totalNum.innerText = `Total de números adicionados: ${val.length}`
-    // Maior e menor número
-    val.sort()
-    let lastNum = Math.max.apply(null, val)
-    let largestNum = document.createElement('p')
-    res.appendChild(largestNum)
-    largestNum.innerText = `Maior valor adicionado: ${lastNum}`
-    let firstNum = Math.min.apply(null, val)
-    let lowestNum = document.createElement('p')
-    res.appendChild(lowestNum)
-    lowestNum.innerText = `Menor valor adicionado: ${firstNum}`
-    // Soma dos números
-    let sum = 0
-    for (let i = 0; i < val.length; i++) {
-        sum += val[i]
+
+    if (val.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        res.innerHTML = ''
+        // Total de números
+        let totalNum = document.createElement('p')
+        res.appendChild(totalNum)
+        totalNum.innerText = `Total de números adicionados: ${val.length}`
+        // Maior e menor número
+        val.sort()
+        let lastNum = Math.max.apply(null, val)
+        let largestNum = document.createElement('p')
+        res.appendChild(largestNum)
+        largestNum.innerText = `Maior valor adicionado: ${lastNum}`
+        let firstNum = Math.min.apply(null, val)
+        let lowestNum = document.createElement('p')
+        res.appendChild(lowestNum)
+        lowestNum.innerText = `Menor valor adicionado: ${firstNum}`
+        // Soma dos números
+        let sum = 0
+        for (let i = 0; i < val.length; i++) {
+            sum += val[i]
+        }
+        let sumOfNum = document.createElement('p')
+        res.appendChild(sumOfNum)
+        sumOfNum.innerText = `A soma de todos os valores adicionados é de: ${sum}`
+        // Média dos valores
+        let average = sum/val.length
+        let avOfNum = document.createElement('p')
+        res.appendChild(avOfNum)
+        avOfNum.innerText = `A média entre os valores adicionados é de: ${average}`
     }
-    let sumOfNum = document.createElement('p')
-    res.appendChild(sumOfNum)
-    sumOfNum.innerText = `A soma de todos os valores adicionados é de: ${sum}`
-    // Média dos valores
-    let average = sum/val.length
-    let avOfNum = document.createElement('p')
-    res.appendChild(avOfNum)
-    avOfNum.innerText = `A média entre os valores adicionados é de: ${average}`
 }
